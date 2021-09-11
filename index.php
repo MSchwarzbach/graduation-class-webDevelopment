14a536259936e99147743867f627dadf5e3ee495
Marilia Schwarzbach <marilia221093@gmail.com>
2021-09-11 19:49:16 -0300

    feat(list clients)

--- index.php
+++ index.php
@@ -23,25 +23,62 @@ $result = $sql->fetchAll();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CRUD Assignment | Web Development</title>
+  <title> CRUD | Tarefa 5 Desenvolvimento Web </title>
   <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
   <link rel="stylesheet" href="assets/bootstrap/all.min.css">
+  <link rel="stylesheet" href="assets/svgs">
   <link rel="icon" href="assets/favicon_fox.ico" type="image/x-icon">
   <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
   <div class="container">
-      <h1>Starting Assigment...</h1>
-      <?php
-        foreach ($result as $r) {
-      ?>
-      <p>
-      <?php echo $r['nome'] ?>
-      </p>
+    <h1> Listagem de Clientes </h1>
+
+    <table class="table table-striped">
+
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Endereço</th>
+          <th>CEP</th>
+          <th>Telefone Celular</th>
+          <th>Telefone Residencial</th>
+          <th>E-mail</th>
+          <th>Sexo</th>
+          <th>Observações</th>
+          <th>Ação</th>
+        </tr>
+      </thead>
+      
+      <tbody>
 
       <?php
-        }
+          foreach ($result as $r) {
       ?>
+
+      <tr>
+          <td> <?php echo $r['nome'] ?> </td>
+          <td> <?php echo $r['endereco'] ?> </td>
+          <td> <?php echo $r['cep'] ?> </td>
+          <td> <?php echo $r['telefone_cel'] ?> </td>
+          <td> <?php echo $r['telefone_res'] ?> </td>
+          <td> <?php echo $r['email'] ?> </td>
+          <td> <?php echo strtoupper($r['sexo']) ?> </td>
+          <td> <?php echo $r['observacoes'] ?> </td>
+          <td>
+            <a href="" class="btn btn-warning">EDITAR</a>
+
+            <a onclick="return confirm('Deseja excluir?')" href="" class="btn btn-danger">DELETAR</a>
+          </td>
+      </tr>
+
+      <?php
+          }
+      ?>
+
+      </tbody>
+      
+    </table>
   </div>
 </body>
 </html>
