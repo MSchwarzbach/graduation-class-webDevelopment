493424e795c8b0c1528e1213db044614ec0183a5
Marilia Schwarzbach <marilia221093@gmail.com>
2021-09-11 18:05:24 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarefa CRUD | Desenvolvimento Web</title>
+    <link rel="stylesheet" href="style/bootstrap.css">
+    <link rel="stylesheet" href="style/all.min.css">
+    <link rel="icon" href="favicon_fox.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style/estilo.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Yo!</h1>
+    </div>
+    
+</body>
+</html>
